d2767d8e64f753f41642f7f5e13ec7a16fd3659b
marcos.vinicius <marcosvinicius.work@hotmail.com>
2021-09-01 06:54:40 -0300

    created class client and logavel

--- src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
@@ -1,5 +1,11 @@
 package one.digitalinnovation.digionebank
 
-class Gerente(nome : String, cpf : String, salario : Double) : Funcionario(nome = nome, cpf = cpf, salario = salario)  {
+class Gerente(nome : String,
+              cpf : String,
+              salario : Double,
+              val senha : String
+) : Funcionario(nome = nome, cpf = cpf, salario = salario), Logavel {
     override fun calculoBonus(): Double = salario * 0.4
+
+    override fun login(): Boolean = "senha123" == senha
 }
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,7 +4,9 @@ import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
 
-    val vitor = Gerente("Vitor Bueno", "123.456.789-11", 5000.0)
+    val vitor = Gerente("Vitor Bueno", "123.456.789-11", 5000.0, "senha123")
     ImprimeRelatorioFuncionario.imprime(vitor)
+
+    TesteAutenticacao().autentica(vitor)
 }
 
